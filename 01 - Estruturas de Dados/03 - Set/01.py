7281dd3b925d5d9905d77fec3f8a2fb67c6b56e7
Josimar <josimarsdepaula@gmail.com>
2024-08-23 19:17:51 -0300

    commit 02

---
+++ 01 - Estruturas de Dados/03 - Set/01.py
@@ -0,0 +1,11 @@
+linguagens = {"python", "josimar", "python", "A", "A"}
+print(linguagens)
+
+carros = set(("Fusca", "Fusca", "fiat"))
+print(carros)
+
+letra = set("abacaxi")
+print(letra)
+
+numeros = set([1,2,3,4,5,5,6,5,6,8,9,8,9,7,5,1])
+print(numeros)
